d73e34f4f276f052940397f417ddd32da07f6fcb
Nathan <nkee@sfu.ca>
2020-06-20 21:04:37 -0700

    test

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 const express = require('express')
 const path = require('path')
-const bodyParser = require('body-parser')
-const app = express()
 const PORT = process.env.PORT || 5000
-const port = 3000
-const db = require('./queries')
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const { Pool } = require('pg');
+var pool;
+pool = new Pool({
+  connectionString: 'postgres://postgres:root@localhost:5432/people'
+})
 
-  get('/', (req,res) => res.render('pages/index'));
-  get('/database', (req,res) => {
-    var getUsersQuery = 'SELECT * FROM Person';
-    pool.query(getUsersQuery, (error, result) => {
-      if (error)
-        res.end(error);
-      var results = {'rows':result.rows}
-      res.render('pages/db',results);
-    })
-  })
+var app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.render('pages/index'));
+app.get('/database', (req, res) => {
+  // var getUsersQuery = 'SELECT * FROM person;';
+  // pool.query(getUsersQuery, (error,result) => {
+  //   if (error) {
+  //     res.end("Query is NULL");
+  //   }
+  //   var results = {'rows':result.rows};
+  //   res.render('pages/db', results);
+  res.render('pages/Assignment2');
+})
+});
+
+app.get('/', (req, res) => res.render('pages/index'));
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
