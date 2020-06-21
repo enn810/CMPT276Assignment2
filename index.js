fadc9f283efec10fb5480f77a54ee1b3f60762d4
Nathan <nkee@sfu.ca>
2020-06-20 18:27:44 -0700

    test1

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser')
+const app = express()
 const PORT = process.env.PORT || 5000
 
 express()
@@ -8,3 +10,18 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
