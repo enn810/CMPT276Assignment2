22fcb5f1b66fb58c0ae9bcf41679c52fd1137f4f
Nathan <nkee@sfu.ca>
2020-06-20 18:58:11 -0700

    test

--- index.js
+++ index.js
@@ -12,26 +12,3 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-
-app.use(bodyParser.json())
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-)
-
-app.get('/', (request, response) => {
-  response.json({ info: 'Node.js, Express, and Postgres API' })
-})
-
-app.get('/users', db.getUsers)
-app.get('/users/:id', db.getUserById)
-app.post('/users', db.createUser)
-app.put('/users/:id', db.updateUser)
-app.delete('/users/:id', db.deleteUser)
-
-
-app.listen(port, () => {
-  console.log(`App running on port ${port}.`)
-})
